f2e00ebfbfc9da74ba2ac7ef93b302202de717e5
thuyhuongctu <thuyhuongctu@users.noreply.github.com>
2026-07-31 09:25:34 +0000

    Deploy BizOn Bật Nghiệp d816caeb9a7fe5a1225e43aae69f98028f126647

--- js/site-dock.js
+++ js/site-dock.js
@@ -40,6 +40,7 @@
     ['Golden Silt Route', 'golden-silt-route.mp3'],
     ['Hộ Chiếu Thương Hiệu – Phần I: Từ dòng Mekong', 'ho-chieu-p1-tu-dong-mekong.mp3'],
     ['Hộ Chiếu Thương Hiệu – Phần II: Qua Những Thị Trường', 'ho-chieu-p2-qua-nhung-thi-truong.mp3'],
+    ['Hộ Chiếu Thương Hiệu – Phần III: Việt Nam ra thế giới', 'ho-chieu-p3-viet-nam-ra-the-gioi.mp3'],
   ];
 
   /* ---------- CSS ---------- */
